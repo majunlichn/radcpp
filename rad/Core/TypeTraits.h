bb120cd0f43bbecaa37631ba18e13a4495054e84
Jerry Ma <majunlichn@outlook.com>
2024-09-24 23:04:39 +0800

    rad/Core/TypeTraits: bit_cast

--- rad/Core/TypeTraits.h
+++ rad/Core/TypeTraits.h
@@ -46,4 +46,23 @@ constexpr auto ToUnderlying(T t) noexcept
     return static_cast<std::underlying_type_t<T>>(t);
 }
 
+// Implementations of std::bit_cast() from C++ 20.
+// Please refer to: https://en.cppreference.com/w/cpp/numeric/bit_cast
+template <class To, class From>
+std::enable_if_t<
+    sizeof(To) == sizeof(From) && std::is_trivially_copyable_v<From>&&
+    std::is_trivially_copyable_v<To>,
+    To>
+    // constexpr support needs compiler magic
+    bit_cast(const From& src) noexcept
+{
+    static_assert(
+        std::is_trivially_constructible_v<To>,
+        "destination type must be trivially constructible");
+
+    To dst;
+    std::memcpy(&dst, &src, sizeof(To));
+    return dst;
+}
+
 } // namespace rad
