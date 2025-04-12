ae6095adb61b6104a76482a01914d70b1122ac2f
Jerry Ma <majunlichn@outlook.com>
2025-04-12 23:16:37 +0800

    rad/Core/RefCounted

---
+++ rad/Core/RefCounted.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <rad/Core/Platform.h>
+
+#include <boost/smart_ptr/intrusive_ptr.hpp>
+#include <boost/smart_ptr/intrusive_ref_counter.hpp>
+
+namespace rad
+{
+
+template<typename T>
+using RefCounted = boost::intrusive_ref_counter<T, boost::thread_unsafe_counter>;
+
+template<typename T>
+using Ref = boost::intrusive_ptr<T>;
+
+} // namespace rad
---
+++ tests/HelloWorld/MemoryTest.cpp
@@ -0,0 +1,31 @@
+#include <rad/Core/Memory.h>
+#include <rad/Core/RefCounted.h>
+#include <atomic>
+
+#include <gtest/gtest.h>
+
+struct A : rad::RefCounted<A>
+{
+    static std::atomic<size_t> InstanceCount;
+    A() { InstanceCount++; }
+    ~A() { InstanceCount--; }
+};
+
+std::atomic<size_t> A::InstanceCount;
+
+void TestRefCounted()
+{
+    rad::Ref<A> a1 = new A();
+    EXPECT_EQ(a1->use_count(), 1);
+    rad::Ref<A> a2 = a1;
+    EXPECT_EQ(a1->use_count(), 2);
+    a1.reset();
+    EXPECT_EQ(a2->use_count(), 1);
+    a2.reset();
+    EXPECT_EQ(A::InstanceCount, 0);
+}
+
+TEST(Core, Memory)
+{
+    TestRefCounted();
+}
