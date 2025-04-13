68b01d03f672c48b7a992e051edf25ff7b864147
Jerry Ma <majunlichn@outlook.com>
2025-04-13 13:22:28 +0800

    rad/IO/Json

---
+++ rad/IO/Json.cpp
@@ -0,0 +1,44 @@
+#include <rad/IO/Json.h>
+#include <rad/IO/File.h>
+#include <rad/Core/String.h>
+
+namespace rad
+{
+
+static void InitJsonParseOptions(boost::json::parse_options& options)
+{
+    options.allow_comments = true;
+    options.allow_trailing_commas = true;
+    options.allow_infinity_and_nan = true;
+}
+
+JsonValue ParseJson(std::string_view str)
+{
+    boost::json::parse_options options = {};
+    InitJsonParseOptions(options);
+    return boost::json::parse(str, {}, options);
+}
+
+JsonValue ParseJsonFromFile(std::string_view fileName)
+{
+    boost::json::parse_options options = {};
+    InitJsonParseOptions(options);
+    return boost::json::parse(File::ReadAll(fileName), {}, options);
+}
+
+JsonValue* FindMemberCaseInsensitive(JsonValue& value, std::string_view key)
+{
+    if (value.is_object())
+    {
+        for (auto& iter : value.get_object())
+        {
+            if (rad::StrCaseEqual(iter.key(), key))
+            {
+                return &iter.value();
+            }
+        }
+    }
+    return nullptr;
+}
+
+} // namespace rad
---
+++ rad/IO/Json.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <rad/Core/Platform.h>
+#include <boost/json.hpp>
+
+namespace rad
+{
+
+using JsonValue = boost::json::value;
+using JsonString = boost::json::string;
+using JsonObject = boost::json::object;
+using JsonArray = boost::json::array;
+
+JsonValue ParseJson(std::string_view str);
+JsonValue ParseJsonFromFile(std::string_view fileName);
+JsonValue* FindMemberCaseInsensitive(JsonValue& value, std::string_view key);
+
+} // namespace rad
