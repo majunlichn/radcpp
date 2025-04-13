643933bde4895cb655181d7745096bd4ec00259f
Jerry Ma <majunlichn@outlook.com>
2025-04-13 13:41:33 +0800

    rad/Core/Float: support fp16, bf16, fp8e4m3 and fp8e5m2

---
+++ rad/Core/BFloat16.cpp
@@ -0,0 +1,56 @@
+#include <rad/Core/Float.h>
+
+namespace rad
+{
+
+uint16_t bf16_from_fp32_round_to_zero(float x)
+{
+    if (!std::isnan(x))
+    {
+        union
+        {
+            float f32;
+            uint32_t u32;
+        };
+        f32 = x;
+        return uint16_t(u32 >> 16);
+    }
+    else
+    {
+        return UINT16_C(0x7FC0);
+    }
+}
+
+// https://github.com/pytorch/pytorch/blob/main/c10/util/BFloat16.h
+uint16_t bf16_from_fp32_round_to_nearest_even(float x)
+{
+    if (!std::isnan(x))
+    {
+        union
+        {
+            float f32;
+            uint32_t u32;
+        };
+        f32 = x;
+        uint32_t roundingBias = ((u32 >> 16) & 1) + UINT32_C(0x7FFF);
+        return static_cast<uint16_t>((u32 + roundingBias) >> 16);
+    }
+    else
+    {
+        return UINT16_C(0x7FC0);
+    }
+}
+
+float bf16_to_fp32(uint16_t x)
+{
+    union
+    {
+        uint32_t u32;
+        float f32;
+    };
+    u32 = x;
+    u32 <<= 16;
+    return f32;
+}
+
+} // namespace rad
